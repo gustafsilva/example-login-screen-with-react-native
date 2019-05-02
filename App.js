df05251cc1cc6e7c48a9d1fe6b1bff332c548c23
Gustavo Freitas <gustavofreitas@localhost.localdomain>
2019-05-02 13:49:28 -0300

    Moving App root.

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+import Application from './src';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <Application />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import LoginScreen from './screens/LoginScreen';
+
+const Application = () => (
+  <LoginScreen />
+);
+
+export default Application;
